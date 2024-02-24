f9a0baaa5f3c708e2a1b21d9640cb38d442594a7
prateeksrivastav2 <prateeksrivastav703@gmail.com>
2024-02-24 11:47:58 +0530

    ready to deploy

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const cors = require("cors");
 const rateLimit = require("express-rate-limit");
 const leetcode = require("./leetCode");
 const app = express();
+const Cors=require('cors');
 const port = 6001;
+// Enable all CORS requests from any origin
+app.use(Cors());
+
+
 
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
-  limit: 600000000000000,
+  limit: 10000000000,
   standardHeaders: "draft-7",
   legacyHeaders: false,
   message: "Too many request from this IP, try again in 1 hour",
